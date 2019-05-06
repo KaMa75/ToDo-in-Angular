21e0fc5ff1934e2e6c7a87ff1fc219e29c8f7019
KaMa75 <karolmarszalek@o2.pl>
2019-05-06 11:56:59 +0200

    ADD implementacja observable

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,31 +1,49 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
-  tasksList: Array<string> = [];
-  doneTasksList: Array<string> = [];
+  private tasksList: Array<string> = [];
+  private doneTasksList: Array<string> = [];
+
+  private tasksListObs = new BehaviorSubject<Array<string>>(this.tasksList);
+  private doneTasksListObs = new BehaviorSubject<Array<string>>(this.doneTasksList);
 
   constructor() {
     this.tasksList = ['Nauka Angulara', 'Zdobycie pracy', 'Zdobywać doświadczenie', 'Zostać FullStackiem'];
+    this.tasksListObs.next(this.tasksList);
   }
 
   addTask(task: string) {
     this.tasksList.push(task);
+    this.tasksListObs.next(this.tasksList);
   }
 
   removeTask(index: number) {
     this.tasksList.splice(index, 1);
+    this.tasksListObs.next(this.tasksList);
   }
 
   doneTask(index: number) {
     const tempTask = this.tasksList.splice(index, 1)[0];
     this.doneTasksList.push(tempTask);
+    this.doneTasksListObs.next(this.doneTasksList);
   }
 
   clearDoneList() {
     this.doneTasksList = [];
+    this.doneTasksListObs.next(this.doneTasksList);
   }
+
+  getTasksListObs(): Observable<Array<string>> {
+    return this.tasksListObs.asObservable();
+  }
+
+  getDoneTasksListObs(): Observable<Array<string>> {
+    return this.doneTasksListObs.asObservable();
+  }
+
 }
