cc0442b27fd6cd435dae1c638a104798fd3d91fa
KaMa75 <karolmarszalek@o2.pl>
2019-05-06 14:50:26 +0200

    ADD dyrektywa wyświetlająca po najechaniu na zadanie jego datę dodania i zakończenia

--- src/app/shared/date.directive.ts
+++ src/app/shared/date.directive.ts
@@ -1,22 +1,27 @@
-import { Directive, HostListener, Input } from '@angular/core';
+import { Directive, HostListener, Input, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appDate]'
 })
 export class DateDirective {
 
-  // @Input() private date: Date;
+  @Input() private date: Date;
 
-  // constructor() { }
+  private paragraph;
 
-  // @HostListener('mouseenter');
-  // mouseenter(eventDate: Event) {
-  //   console.log(this.date);
-  // }
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    this.paragraph = this.renderer.createElement('p');
+  }
 
-  // @HostListener('mouseleave');
-  // mouseleave(eventDate: Event) {
-  //   console.log('mouseleave');
-  // }
+  @HostListener('mouseenter')
+  mouseenter(eventDate: Event) {
+    this.paragraph.innerText = this.date.toLocaleDateString();
+    this.renderer.appendChild(this.el.nativeElement, this.paragraph);
+  }
+
+  @HostListener('mouseleave')
+  mouseleave(eventDate: Event) {
+    this.renderer.removeChild(this.el.nativeElement, this.paragraph);
+  }
 
 }
